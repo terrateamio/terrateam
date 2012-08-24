010295f1bdc6e9248f0a6e9f6ca3f7362d252210
Anil Madhavapeddy <anil@recoil.org>
2012-08-23 23:18:22 -0700

    sync OASIS files

--- vendor/ocaml-uri/myocamlbuild.ml
+++ vendor/ocaml-uri/myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: ec275c913f409ecb31c226781c7fed84) *)
+(* DO NOT EDIT (digest: f20c9c46e20d234051cf685008834df0) *)
 module OASISGettext = struct
 # 21 "/Users/avsm/.opam/system/build/oasis.0.3.0/src/oasis/OASISGettext.ml"
 
@@ -477,7 +477,8 @@ end
 open Ocamlbuild_plugin;;
 let package_default =
   {
-     MyOCamlbuildBase.lib_ocaml = [("uri", ["lib"])];
+     MyOCamlbuildBase.lib_ocaml =
+       [("uri", ["lib"]); ("services", ["lib"]); ("services_full", ["lib"])];
      lib_c = [];
      flags = [];
      includes = [("lib_test", ["lib"])];
@@ -486,6 +487,6 @@ let package_default =
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default package_default;;
 
-# 490 "myocamlbuild.ml"
+# 491 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
