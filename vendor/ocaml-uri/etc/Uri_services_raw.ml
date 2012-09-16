674af46b3f137a156c22367ec4d0ce9c45dd2136
Anil Madhavapeddy <anil@recoil.org>
2012-09-16 21:11:18 +0100

    port_of_uri should return a port specified by the URI if present in the authority
    bug noticed by @dsheets

--- vendor/ocaml-uri/etc/Uri_services_raw.ml
+++ vendor/ocaml-uri/etc/Uri_services_raw.ml
@@ -1,12 +1,16 @@
 let port_of_uri ?default lookupfn uri =
-  match Uri.scheme uri, default with
-  |None, None -> None
-  |None, Some scheme
-  |Some scheme, _ -> begin
-    match lookupfn scheme with
-    |[] -> None
-    |hd::_ -> Some hd
-   end
+  match Uri.port uri with
+  |Some port as x -> x
+  |None -> begin
+    match Uri.scheme uri, default with
+    |None, None -> None
+    |None, Some scheme
+    |Some scheme, _ -> begin
+      match lookupfn scheme with
+      |[] -> None
+      |hd::_ -> Some hd
+     end
+  end
 
 let tcp_port_of_uri ?default uri =
   port_of_uri ?default tcp_port_of_service uri
