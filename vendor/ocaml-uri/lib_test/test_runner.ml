5b5178470c7ba66e26a4022437c3cea7d3a60251
Vincent Bernardoff <vb@luminar.eu.org>
2014-06-25 20:42:00 +0200

    Added a test case for IPv6 litteral with zone id

--- vendor/ocaml-uri/lib_test/test_runner.ml
+++ vendor/ocaml-uri/lib_test/test_runner.ml
@@ -57,8 +57,16 @@ let uri_encodes = [
   "http://foo%21.com", (Uri.make ~scheme:"http" ~host:"foo!.com" ());
   "/wh/at/ev/er", (Uri.make ~path:"/wh/at/ev/er" ());
   "/wh/at!/ev%20/er", (Uri.make ~path:"/wh/at!/ev /er" ());
+  (* IPv6 litteral *)
   "http://%5Bdead%3Abeef%3A%3Adead%3A0%3Abeaf%5D",
     (Uri.make ~scheme:"http" ~host:"[dead:beef::dead:0:beaf]" ());
+  "http://user:pass@%5B2001%3A41d1%3Afe67%3A500%3A227%3Ac6ff%3Afe5a%3Aefa0%5D:6789/wh/at/ever?foo=1&bar=5#5",
+  (Uri.make ~scheme:"http" ~userinfo:"user:pass" ~host:"[2001:41d1:fe67:500:227:c6ff:fe5a:efa0]"
+     ~port:6789 ~path:"/wh/at/ever" ~query:["foo",["1"];"bar",["5"]] ~fragment:"5" ());
+  (* IPv6 litteral with zone id *)
+  "http://user:pass@%5B2001%3A41d1%3Afe67%3A500%3A227%3Ac6ff%3Afe5a%3Aefa0%25wlan0%5D:6789/wh/at/ever?foo=1&bar=5#5",
+  (Uri.make ~scheme:"http" ~userinfo:"user:pass" ~host:"[2001:41d1:fe67:500:227:c6ff:fe5a:efa0%wlan0]"
+     ~port:6789 ~path:"/wh/at/ever" ~query:["foo",["1"];"bar",["5"]] ~fragment:"5" ());
   "foo+bar%3A", (Uri.make ~path:"foo+bar:" ());
   "foo+bar:///", (Uri.make ~scheme:"foo+bar" ~host:"" ~path:"/" ());
   "foo2-bar.baz:///", (Uri.make ~scheme:"foo2-bar.baz" ~host:"" ~path:"/" ());
