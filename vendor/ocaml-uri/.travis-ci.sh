66b99d90bba4f7e9a542d7187f1bf4e746480289
Anil Madhavapeddy <anil@recoil.org>
2014-12-16 17:07:27 -0800

    Drop support for OCaml 3.12.1, and now require OCaml 4.00.1+
    This removes the toplevel libs hack from the _oasis file

--- vendor/ocaml-uri/.travis-ci.sh
+++ vendor/ocaml-uri/.travis-ci.sh
@@ -1,12 +1,9 @@
 OPAM_DEPENDS="ocamlfind ounit re sexplib stringext"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
-4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
+4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
@@ -28,11 +25,4 @@ opam install ${OPAM_DEPENDS}
 eval `opam config env`
 make
 make test
-opam pin uri .
-
-case $OCAML_VERSION in
-3.12.*) ;;
-*)
-  opam install mirage-www
-  ;;
-esac
+opam pin add uri .
