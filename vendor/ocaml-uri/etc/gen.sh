4de94daf8900a035a176ccc9aa22864845bbd845
David Sheets <sheets@alum.mit.edu>
2015-05-13 16:26:25 +0100

    Fix etc/gen.sh to work with stdlibs without bytes

--- vendor/ocaml-uri/etc/gen.sh
+++ vendor/ocaml-uri/etc/gen.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -e
 
+BYTES=`ocamlfind query bytes`
 STRINGEXT=`ocamlfind query stringext`
-DEPS="-I ${STRINGEXT} stringext.cma"
+DEPS="-I ${BYTES} bytes.cma -I ${STRINGEXT} stringext.cma"
 ocaml ${DEPS} etc/gen_services.ml etc/services.short > lib/uri_services.ml
 cat etc/uri_services_raw.ml >> lib/uri_services.ml
 ocaml ${DEPS} etc/gen_services.ml etc/services.full > lib/uri_services_full.ml
