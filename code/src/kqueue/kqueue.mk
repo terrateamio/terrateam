f41ec2d353dae59e3c2dd20d41dc607c398f1d09
orbitz <orbitz@gmail.com>
2016-12-30 21:56:06 +0100

    Upgrade to latest hll and pds

--- code/src/kqueue/kqueue.mk
+++ code/src/kqueue/kqueue.mk
@@ -1,9 +1,12 @@
 .PHONY: clean_kqueue_bindings
 
+# Modifying NON_LIB_MODULES, so make it serial
+.NOTPARALLEL:
+
 NON_LIB_MODULES += kqueue_stubs.ml
 
-kqueue_stubs.ml: kqueue_bindings_gen
-	./$^ > $@
+$(SRC_DIR)/kqueue_stubs.ml: kqueue_bindings_gen
+	./$^ > $@.$$$$ && mv $@.$$$$ $@
 
 kqueue_bindings_gen: kqueue_bindings_stubs.o
 	$(CC) -o $@ $^
@@ -19,4 +22,5 @@ clean: clean_kqueue_bindings
 clean_kqueue_bindings:
 	-rm kqueue_bindings_gen \
 		kqueue_bindings_stubs.o \
-		kqueue_bindings_stubs.c
+		kqueue_bindings_stubs.c \
+		$(SRC_DIR)/kqueue_stubs.ml
