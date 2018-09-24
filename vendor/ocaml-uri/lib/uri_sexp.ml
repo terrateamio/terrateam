6450701167048531e7cdf6fb8be45f81ccefba90
Anil Madhavapeddy <anil@recoil.org>
2018-09-24 10:49:59 +0100

    simplify and reduce the size of the sexp representation
    also fix the test cases and move them to dune
    
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- vendor/ocaml-uri/lib/uri_sexp.ml
+++ vendor/ocaml-uri/lib/uri_sexp.ml
@@ -18,13 +18,13 @@ struct
 	] [@@deriving sexp]
 
 	type t = {
-	  s_scheme: string sexp_option;
-	  s_userinfo: string sexp_option;
-	  s_host: string sexp_option;
-	  s_port: int sexp_option;
-	  s_path: string;
-	  s_query: (string * string list) list;
-	  s_fragment: string sexp_option
+          scheme: string option [@default None] [@sexp_drop_default];
+          userinfo: string option [@default None] [@sexp_drop_default];
+          host: string option [@default None] [@sexp_drop_default];
+          port: int option [@default None] [@sexp_drop_default];
+          path: string [@default ""] [@sexp_drop_default];
+          query: (string * string list) sexp_list;
+          fragment: string option [@default None] [@sexp_drop_default]
 	} [@@deriving sexp]
 
 end
@@ -37,22 +37,22 @@ let sexp_of_component = sexp_of_component
 let t_of_sexp sexp =
 	let t = t_of_sexp sexp in
 	Uri.make
-		?scheme:t.s_scheme
-		?userinfo:t.s_userinfo
-		?host:t.s_host
-		?port:t.s_port
-		~path:t.s_path
-		~query:t.s_query
-		?fragment:t.s_fragment
+		?scheme:t.scheme
+		?userinfo:t.userinfo
+		?host:t.host
+		?port:t.port
+		~path:t.path
+		~query:t.query
+		?fragment:t.fragment
 		()
 
 let sexp_of_t t =
 	sexp_of_t {
-		s_scheme = scheme t;
-		s_userinfo = userinfo t;
-		s_host = host t;
-		s_port = port t;
-		s_path = path t;
-		s_query = query t;
-		s_fragment = fragment t
+		scheme = scheme t;
+		userinfo = userinfo t;
+		host = host t;
+		port = port t;
+		path = path t;
+		query = query t;
+		fragment = fragment t
 	}
