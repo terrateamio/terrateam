21d1e1655bfe382650721bca6fe57a817491f612
Malcolm Matalka <malcolm.matalka@acsl.se>
2019-03-14 17:25:13 +0100

    ADD Log the digest of the session as an extra key

--- code/src/brtl_mw_log/brtl_mw_log.ml
+++ code/src/brtl_mw_log/brtl_mw_log.ml
@@ -1,5 +1,7 @@
 module Config = struct
-  type t = { remote_ip_header : string option }
+  type t = { remote_ip_header : string option
+           ; extra_key : ((string, Brtl_rspnc.t) Brtl_ctx.t -> string option)
+           }
 end
 
 let pre_handler = Brtl_mw.pre_handler_noop
@@ -18,7 +20,13 @@ let post_handler config ctx =
       ~default:(Brtl_ctx.remote_addr ctx)
       (CCOpt.flat_map (Cohttp.Header.get headers) config.Config.remote_ip_header)
   in
-  Logs.info (fun m -> m "%s %s %s %s %s"
+  let extra_key =
+    match config.Config.extra_key ctx with
+      | Some s -> s
+      | None -> ""
+  in
+  Logs.info (fun m -> m "%s : %s : %s : %s : %s : %s"
+                extra_key
                 remote_addr
                 token
                 meth
--- code/src/brtl_mw_log/brtl_mw_log.mli
+++ code/src/brtl_mw_log/brtl_mw_log.mli
@@ -1,5 +1,7 @@
 module Config : sig
-  type t = { remote_ip_header : string option }
+  type t = { remote_ip_header : string option
+           ; extra_key : ((string, Brtl_rspnc.t) Brtl_ctx.t -> string option)
+           }
 end
 
 val create : Config.t -> Brtl_mw.Mw.t
