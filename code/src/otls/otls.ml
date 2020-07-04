6468bd6091dfc5e3ce42fb857cb926f7b8946fa0
Malcolm Matalka <malcolm.matalka@acsl.se>
2020-07-04 18:26:51 +0200

    REFACTOR Format otls

--- code/src/otls/otls.ml
+++ code/src/otls/otls.ml
@@ -1,7 +1,12 @@
-module Stubs = Otls_bindings.Stubs(Otls_stubs)
+module Stubs = Otls_bindings.Stubs (Otls_stubs)
 
 type err = [ `Error ]
-type io_err = [ err | `Want_pollin | `Want_pollout ]
+
+type io_err =
+  [ err
+  | `Want_pollin
+  | `Want_pollout
+  ]
 
 module Tls_protocols = struct
   type t =
@@ -27,13 +32,13 @@ module Tls_ciphers = struct
     | Default
     | Legacy
     | Insecure
-    | Ciphers of string list
+    | Ciphers  of string list
 
   let to_string = function
-    | Secure -> "secure"
-    | Default -> "default"
-    | Legacy -> "legacy"
-    | Insecure -> "insecure"
+    | Secure        -> "secure"
+    | Default       -> "default"
+    | Legacy        -> "legacy"
+    | Insecure      -> "insecure"
     | Ciphers cphrs -> String.concat ":" cphrs
 end
 
@@ -46,112 +51,85 @@ module Tls_config = struct
 
   let set_ca_file t fname =
     match Otls_c_bindings.Tls_config.tls_config_set_ca_file t fname with
-      | -1 ->
-        Error `Error
-      | _ ->
-        Ok ()
+      | -1 -> Error `Error
+      | _  -> Ok ()
 
   let set_cert_file t fname =
     match Otls_c_bindings.Tls_config.tls_config_set_cert_file t fname with
-      | -1 ->
-        Error `Error
-      | _ ->
-        Ok ()
+      | -1 -> Error `Error
+      | _  -> Ok ()
 
   let set_key_file t fname =
     match Otls_c_bindings.Tls_config.tls_config_set_key_file t fname with
-      | -1 ->
-        Error `Error
-      | _ ->
-        Ok ()
+      | -1 -> Error `Error
+      | _  -> Ok ()
 
-  let verify t =
-    Otls_c_bindings.Tls_config.tls_config_verify t
+  let verify t = Otls_c_bindings.Tls_config.tls_config_verify t
 
-  let insecure_noverifycert t =
-    Otls_c_bindings.Tls_config.tls_config_insecure_noverifycert t
+  let insecure_noverifycert t = Otls_c_bindings.Tls_config.tls_config_insecure_noverifycert t
 
-  let insecure_noverifyname t =
-    Otls_c_bindings.Tls_config.tls_config_insecure_noverifyname t
+  let insecure_noverifyname t = Otls_c_bindings.Tls_config.tls_config_insecure_noverifyname t
 
-  let insecure_noverifytime t =
-    Otls_c_bindings.Tls_config.tls_config_insecure_noverifytime t
+  let insecure_noverifytime t = Otls_c_bindings.Tls_config.tls_config_insecure_noverifytime t
 
   let set_protocols t protos =
     let bitmask =
-      ListLabels.fold_left
-        ~f:(fun bm v -> bm lor (Tls_protocols.to_int v))
-        ~init:0
-        protos
+      ListLabels.fold_left ~f:(fun bm v -> bm lor Tls_protocols.to_int v) ~init:0 protos
     in
     let ret =
-      Otls_c_bindings.Tls_config.tls_config_set_protocols
-        t
-        (Unsigned.UInt32.of_int bitmask)
+      Otls_c_bindings.Tls_config.tls_config_set_protocols t (Unsigned.UInt32.of_int bitmask)
     in
     assert (ret = 0);
     ()
 
   let set_ciphers t ciphers =
-    let ret =
-      Otls_c_bindings.Tls_config.tls_config_set_ciphers
-        t
-        (Tls_ciphers.to_string ciphers)
-    in
+    let ret = Otls_c_bindings.Tls_config.tls_config_set_ciphers t (Tls_ciphers.to_string ciphers) in
     match ret with
       | -1 -> Error `Error
-      | _ -> Ok ()
+      | _  -> Ok ()
 end
 
 module Tls = struct
   type server
+
   type client
+
   type 'a t = Otls_c_bindings.Tls.t
 
   let client () = Otls_c_bindings.Tls.tls_client ()
 
   let server () = Otls_c_bindings.Tls.tls_server ()
 
-  let destroy t = Otls_c_bindings.Tls.tls_free t
+  let destroy t =
+    ignore (Otls_c_bindings.Tls.tls_close t);
+    Otls_c_bindings.Tls.tls_free t
 
   let connect_socket t socket servername =
     match Otls_c_bindings.Tls.tls_connect_socket t socket servername with
-      | -1 ->
-        Error `Error
-      | _ ->
-        Ok ()
+      | -1 -> Error `Error
+      | _  -> Ok ()
 
   let accept_socket t socket =
     let tls_ptr = Ctypes.(allocate_n Otls_c_bindings.Tls.t ~count:1) in
     match Otls_c_bindings.Tls.tls_accept_socket t tls_ptr socket with
-      | -1 ->
-        Error `Error
-      | _ ->
-        Ok Ctypes.(!@ tls_ptr)
+      | -1 -> Error `Error
+      | _  -> Ok Ctypes.(!@tls_ptr)
 
   let read t ~pos ~len bytes =
     let buf = Ctypes.(ocaml_bytes_start bytes +@ pos) in
     match Otls_c_bindings.Tls.tls_read t buf (Unsigned.Size_t.of_int len) with
-      | n when n = PosixTypes.Ssize.minus_one ->
-        Error `Error
-      | n when n = Stubs.tls_want_pollin ->
-        Error `Want_pollin
-      | n when n = Stubs.tls_want_pollout ->
-        Error `Want_pollout
-      | n ->
-        Ok (PosixTypes.Ssize.to_int n)
+      | n when n = PosixTypes.Ssize.minus_one -> Error `Error
+      | n when n = Stubs.tls_want_pollin -> Error `Want_pollin
+      | n when n = Stubs.tls_want_pollout -> Error `Want_pollout
+      | n -> Ok (PosixTypes.Ssize.to_int n)
 
   let write t ~pos ~len bytes =
     let buf = Ctypes.(ocaml_bytes_start bytes +@ pos) in
     match Otls_c_bindings.Tls.tls_write t buf (Unsigned.Size_t.of_int len) with
-      | n when n = PosixTypes.Ssize.minus_one ->
-        Error `Error
-      | n when n = Stubs.tls_want_pollin ->
-        Error `Want_pollin
-      | n when n = Stubs.tls_want_pollout ->
-        Error `Want_pollout
-      | n ->
-        Ok (PosixTypes.Ssize.to_int n)
+      | n when n = PosixTypes.Ssize.minus_one -> Error `Error
+      | n when n = Stubs.tls_want_pollin -> Error `Want_pollin
+      | n when n = Stubs.tls_want_pollout -> Error `Want_pollout
+      | n -> Ok (PosixTypes.Ssize.to_int n)
 
   let error = Otls_c_bindings.Tls.tls_error
 
@@ -179,15 +157,11 @@ end
 
 let configure tls tls_config =
   match Otls_c_bindings.tls_configure tls tls_config with
-    | -1 ->
-      Error `Error
-    | _ ->
-      Ok ()
+    | -1 -> Error `Error
+    | _  -> Ok ()
 
 (* Initialize TLS. *)
 let () =
   match Otls_c_bindings.tls_init () with
-    | -1 ->
-      assert false
-    | _ ->
-      ()
+    | -1 -> assert false
+    | _  -> ()
