869a23ee7f27d7ce1955fdcb2477f4640c512636
Malcolm Matalka <malcolm.matalka@acsl.se>
2020-07-12 20:10:43 +0200

    FIX Abb_test_concurrency failing due to timing

--- code/src/abb_test/abb_test_concurrency.ml
+++ code/src/abb_test/abb_test_concurrency.ml
@@ -5,14 +5,14 @@ module Make (Abb : Abb_intf.S) = struct
     Oth_abb.test ~desc:"Applicatives run concurrently" ~name:"Applicative Concurrent" (fun () ->
         let open Abb.Future.Infix_monad in
         let open Abb.Future.Infix_app in
-        let start_time = Unix.time () in
+        let start_time = Unix.gettimeofday () in
         let fut1 = Abb.Sys.sleep 1.0 in
         let fut2 = Abb.Sys.sleep 1.0 in
         (fun () () -> ())
         <$> fut1
         <*> fut2
         >>| fun () ->
-        let end_time = Unix.time () in
+        let end_time = Unix.gettimeofday () in
         assert (end_time -. start_time < 1.8))
 
   let test = Oth_abb.serial [ test_applicative ]
