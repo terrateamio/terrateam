c725c1e11bb6700f3d0aff2eec1fbbcf4a7b307b
Malcolm Matalka <malcolm.matalka@acsl.se>
2020-07-17 17:59:06 +0200

    FIX Make Otls generate files in a separate directory and build there
    This is important so release/debug/profile builds don't step on each other

--- code/src/otls/otls.mk
+++ code/src/otls/otls.mk
@@ -3,10 +3,23 @@
 # Modifying NON_LIB_MODULES, so make it serial
 .NOTPARALLEL:
 
+OLD_SRC_DIR := $(SRC_DIR)
+
+SRC_DIR = src
+
 NON_LIB_MODULES += otls_stubs.ml
 
 $(SRC_DIR)/otls_stubs.ml: otls_bindings_gen
+	mkdir -p "$(SRC_DIR)"
 	./$^ > $@.$$$$ && mv $@.$$$$ $@
+
+$(SRC_DIR)/%.ml: $(OLD_SRC_DIR)/%.ml
+	mkdir -p "$(SRC_DIR)"
+	cp $^ $@
+
+$(SRC_DIR)/%.mli: $(OLD_SRC_DIR)/%.mli
+	mkdir -p "$(SRC_DIR)"
+	cp $^ $@
 
 otls_bindings_gen: otls_bindings_stubs.o
 	$(CC) $(shell pkg-config --cflags libtls) -o $@ $^
