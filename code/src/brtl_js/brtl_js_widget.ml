e2b6b7fe2d1b629c88c94069742b736b77e5cc20
Malcolm Matalka <malcolm.matalka@acsl.se>
2021-01-02 18:23:54 +0100

    ADD Proma Settings screen plus refactor how the sticky is done to work better

--- code/src/brtl_js/brtl_js_widget.ml
+++ code/src/brtl_js/brtl_js_widget.ml
@@ -1,5 +1,14 @@
 open Js_of_ocaml
 
+type 'a t = {
+  signal : 'a Brtl_js.React.signal;
+  set : ?step:Brtl_js.React.step -> 'a -> unit;
+}
+
+let signal t = t.signal
+
+let set ?step t v = t.set ?step v
+
 let input ?(a = []) ?(value = "") () =
   let (elem_value, elem_set_value) = Brtl_js.React.S.create value in
   let onkeyup =
@@ -16,4 +25,26 @@ let input ?(a = []) ?(value = "") () =
     elem##.value := Js.string s;
     elem_set_value ?step s
   in
-  (elem_value, set_value, elem)
+  ({ signal = elem_value; set = set_value }, elem)
+
+let checkbox ?(a = []) ?(value = false) () =
+  let (elem_value, elem_set_value) = Brtl_js.React.S.create value in
+  let onchange =
+    Brtl_js.handler_sync (fun event ->
+        Js.Opt.iter event##.target (fun target ->
+            Js.Opt.iter (Dom_html.CoerceTo.input target) (fun inp ->
+                elem_set_value (Js.to_bool inp##.checked))))
+  in
+  let a =
+    if value then
+      Brtl_js.Html.a_checked () :: Brtl_js.Html.a_onchange onchange :: a
+    else
+      Brtl_js.Html.a_onchange onchange :: a
+  in
+  let elem = Brtl_js.Html.input ~a:(Brtl_js.Html.a_input_type `Checkbox :: a) () in
+  let set_value ?step b =
+    let elem = Brtl_js.To_dom.of_input elem in
+    elem##.checked := Js.bool b;
+    elem_set_value ?step b
+  in
+  ({ signal = elem_value; set = set_value }, elem)
--- code/src/brtl_js/brtl_js_widget.mli
+++ code/src/brtl_js/brtl_js_widget.mli
@@ -1,7 +1,17 @@
+type 'a t
+
+val signal : 'a t -> 'a Brtl_js.React.signal
+
+val set : ?step:Brtl_js.React.step -> 'a t -> 'a -> unit
+
 val input :
   ?a:Html_types.input_attrib Brtl_js.Html.attrib list ->
   ?value:string ->
   unit ->
-  string Brtl_js.React.signal
-  * (?step:Brtl_js.React.step -> string -> unit)
-  * [> Html_types.input ] Brtl_js.Html.elt
+  string t * [> Html_types.input ] Brtl_js.Html.elt
+
+val checkbox :
+  ?a:Html_types.input_attrib Brtl_js.Html.attrib list ->
+  ?value:bool ->
+  unit ->
+  bool t * [> Html_types.input ] Brtl_js.Html.elt
