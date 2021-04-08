f4acb3ebe68e57b4e5a87ef05ee75b5551a9e803
Malcolm Matalka <malcolm.matalka@acsl.se>
2021-04-08 11:12:26 +0200

    REFACTOR Localforage Add typed interface

--- code/src/localforage/localforage.ml
+++ code/src/localforage/localforage.ml
@@ -24,31 +24,50 @@ class type store =
     method keys : Js.js_string Js.js_array Js.t Abb_fut_js.promise Js.t Js.meth
   end
 
-type t = store Js.t
+let global : unit -> store Js.t = fun _ -> Js.Unsafe.global##.localforage
 
-let global : unit -> t = fun _ -> Js.Unsafe.global##.localforage
+module Typed = struct
+  type 'a t = {
+    store : store Js.t;
+    encode : 'a -> string;
+    decode : string -> 'a option;
+  }
 
-let create name =
-  (global ())##createInstance
-    (object%js
-       val name = Js.string name
-    end)
+  let create ~encode ~decode name =
+    let store =
+      (global ())##createInstance
+        (object%js
+           val name = Js.string name
+        end)
+    in
+    { store; encode; decode }
 
-let destroy t = Abb_fut_js.unsafe_of_promise t##dropInstance
+  let destroy t = Abb_fut_js.unsafe_of_promise t.store##dropInstance
 
-let set_item t ~key value = Abb_fut_js.unsafe_of_promise (t##setItem (Js.string key) value)
+  let set_item t ~key v =
+    Abb_fut_js.unsafe_of_promise
+      (t.store##setItem (Js.string key) (Js.Unsafe.inject (Js.string (t.encode v))))
 
-let get_item t key =
-  let open Abb_fut_js.Infix_monad in
-  Abb_fut_js.unsafe_of_promise (t##getItem (Js.string key)) >>| Js.Opt.to_option
+  let get_item t key =
+    let open Abb_fut_js.Infix_monad in
+    Abb_fut_js.unsafe_of_promise (t.store##getItem (Js.string key))
+    >>| fun s ->
+    CCOpt.flat_map CCFun.(Js.Unsafe.coerce %> Js.to_string %> t.decode) (Js.Opt.to_option s)
 
-let remove_item t key = Abb_fut_js.unsafe_of_promise (t##removeItem (Js.string key))
+  let remove_item t key = Abb_fut_js.unsafe_of_promise (t.store##removeItem (Js.string key))
 
-let clear t = Abb_fut_js.unsafe_of_promise t##clear
+  let clear t = Abb_fut_js.unsafe_of_promise t.store##clear
 
-let length t = Abb_fut_js.unsafe_of_promise t##length
+  let length t = Abb_fut_js.unsafe_of_promise t.store##length
 
-let keys t =
-  let open Abb_fut_js.Infix_monad in
-  Abb_fut_js.unsafe_of_promise t##keys
-  >>| fun keys -> keys |> Js.to_array |> Array.to_list |> List.map Js.to_string
+  let keys t =
+    let open Abb_fut_js.Infix_monad in
+    Abb_fut_js.unsafe_of_promise t.store##keys
+    >>| fun keys -> keys |> Js.to_array |> Array.to_list |> List.map Js.to_string
+end
+
+include Typed
+
+type t = string Typed.t
+
+let create name = Typed.create ~encode:CCFun.id ~decode:CCOpt.return name
--- code/src/localforage/localforage.mli
+++ code/src/localforage/localforage.mli
@@ -1,14 +1,32 @@
 type t
 
-val global : unit -> t
+module Typed : sig
+  type 'a t
+
+  val create : encode:('a -> string) -> decode:(string -> 'a option) -> string -> 'a t
+
+  val destroy : 'a t -> unit Abb_fut_js.t
+
+  val set_item : 'a t -> key:string -> 'a -> unit Abb_fut_js.t
+
+  val get_item : 'a t -> string -> 'a option Abb_fut_js.t
+
+  val remove_item : 'a t -> string -> unit Abb_fut_js.t
+
+  val clear : 'a t -> unit Abb_fut_js.t
+
+  val length : 'a t -> int Abb_fut_js.t
+
+  val keys : 'a t -> string list Abb_fut_js.t
+end
 
 val create : string -> t
 
 val destroy : t -> unit Abb_fut_js.t
 
-val set_item : t -> key:string -> Js_of_ocaml.Js.Unsafe.any -> unit Abb_fut_js.t
+val set_item : t -> key:string -> string -> unit Abb_fut_js.t
 
-val get_item : t -> string -> Js_of_ocaml.Js.Unsafe.any option Abb_fut_js.t
+val get_item : t -> string -> string option Abb_fut_js.t
 
 val remove_item : t -> string -> unit Abb_fut_js.t
 
