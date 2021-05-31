46a6192a36a56275a52b06fc68e668b76a0c46a1
Malcolm Matalka <malcolm.matalka@acsl.se>
2021-05-31 22:47:17 +0200

    ADD Brtl_js Datetime functionality

---
+++ code/src/brtl_js/brtl_js_datetime.ml
@@ -0,0 +1,38 @@
+module Js = Js_of_ocaml.Js
+
+type t = Js.date Js.t
+
+let millisecond_day = 24.0 *. 60.0 *. 60.0 *. 1000.0
+
+let constr = Js.Unsafe.global##._Date
+
+let of_str_constr : (Js.js_string Js.t -> Js.date Js.t) Js.constr = constr
+
+let of_string s = new%js of_str_constr (Js.string s)
+
+let to_iso_string t = Js.to_string t##toISOString
+
+let to_yyyy_mm_dd_hh_mm d =
+  Printf.sprintf
+    "%04d-%02d-%02d %02d:%02d"
+    d##getFullYear
+    (d##getMonth + 1)
+    d##getDate
+    d##getHours
+    d##getMinutes
+
+let to_yyyy_mm_dd d = Printf.sprintf "%04d-%02d-%02d" d##getFullYear (d##getMonth + 1) d##getDate
+
+let to_yyyy_mm d = Printf.sprintf "%04d-%02d" d##getFullYear (d##getMonth + 1)
+
+let rec range from until =
+  if from##getTime < until##getTime then
+    from :: range (new%js Js.date_fromTimeValue (from##getTime +. millisecond_day)) until
+  else
+    []
+
+let add_milliseconds d ms = new%js Js.date_fromTimeValue (d##getTime +. ms)
+
+let now () = new%js Js.date_now
+
+let get_time d = d##getTime
---
+++ code/src/brtl_js/brtl_js_datetime.mli
@@ -0,0 +1,21 @@
+type t
+
+val millisecond_day : float
+
+val of_string : string -> t
+
+val to_iso_string : t -> string
+
+val to_yyyy_mm_dd_hh_mm : t -> string
+
+val to_yyyy_mm_dd : t -> string
+
+val to_yyyy_mm : t -> string
+
+val range : t -> t -> t list
+
+val add_milliseconds : t -> float -> t
+
+val now : unit -> t
+
+val get_time : t -> float
