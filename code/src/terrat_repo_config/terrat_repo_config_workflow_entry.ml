50c495ab28e18cd2df28374c2f5e75b61abe8ba6
Malcolm <malcolm@terrateam.io>
2022-06-16 20:53:42 -0400

    FIX Terrat Move terragrunt and terraform version config up level

--- code/src/terrat_repo_config/terrat_repo_config_workflow_entry.ml
+++ code/src/terrat_repo_config/terrat_repo_config_workflow_entry.ml
@@ -2,5 +2,7 @@ type t = {
   apply : Terrat_repo_config_workflow_op_list.t option; [@default None]
   plan : Terrat_repo_config_workflow_op_list.t option; [@default None]
   tag_query : string;
+  terraform_version : Terrat_repo_config_terraform_version.t option; [@default None]
+  terragrunt : bool; [@default false]
 }
 [@@deriving yojson { strict = true; meta = true }, make, show]
