3bd0e86704f25c53e6a162704619b31c8ab807ce
Malcolm <malcolm@terrateam.io>
2022-07-21 10:00:14 +0200

    ADD Terrat Support workflow run in progress events

--- code/src/terrat_github_webhooks/terrat_github_webhooks_workflow_run_event.ml
+++ code/src/terrat_github_webhooks/terrat_github_webhooks_workflow_run_event.ml
@@ -1,6 +1,7 @@
 type t =
   | Workflow_run_completed of Terrat_github_webhooks_workflow_run_completed.t
   | Workflow_run_requested of Terrat_github_webhooks_workflow_run_requested.t
+  | Workflow_run_in_progress of Terrat_github_webhooks_workflow_run_in_progress.t
 [@@deriving show]
 
 let of_yojson =
@@ -15,8 +16,13 @@ let of_yojson =
         map
           (fun v -> Workflow_run_requested v)
           (Terrat_github_webhooks_workflow_run_requested.of_yojson v));
+      (fun v ->
+        map
+          (fun v -> Workflow_run_in_progress v)
+          (Terrat_github_webhooks_workflow_run_in_progress.of_yojson v));
     ])
 
 let to_yojson = function
   | Workflow_run_completed v -> Terrat_github_webhooks_workflow_run_completed.to_yojson v
   | Workflow_run_requested v -> Terrat_github_webhooks_workflow_run_requested.to_yojson v
+  | Workflow_run_in_progress v -> Terrat_github_webhooks_workflow_run_in_progress.to_yojson v
