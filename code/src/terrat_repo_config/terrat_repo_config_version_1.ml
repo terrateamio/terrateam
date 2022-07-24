cffd4d07d957c287c80609df5c2cca798b65ad2b
Malcolm <malcolm@terrateam.io>
2022-07-24 07:57:41 +0200

    ADD Terrat Workspaces configuration to top-level

--- code/src/terrat_repo_config/terrat_repo_config_version_1.ml
+++ code/src/terrat_repo_config/terrat_repo_config_version_1.ml
@@ -56,6 +56,7 @@ type t = {
   automerge : Terrat_repo_config_automerge.t option; [@default None]
   checkout_strategy : Checkout_strategy.t; [@default "merge"]
   cost_estimation : Cost_estimation.t option; [@default None]
+  create_and_select_workspace : bool; [@default true]
   default_tf_version : Terrat_repo_config_terraform_version.t option; [@default None]
   dirs : Dirs.t option; [@default None]
   enabled : bool; [@default true]
