2afe9e2c07028657f5954728187e73b25abef15d
Josh Pollara <joshpollara@gmail.com>
2023-05-21 21:31:29 +0200

    PRO-335 FIX docker push to ghcr

--- docker/terrat/build-self-hosted.sh
+++ docker/terrat/build-self-hosted.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eufx -o pipefail
 
-echo "${TERRATEAM_SELF_HOSTED_BUILD_GH_TOKEN}" | docker login ghcr.io -u $ --password-stdin
+echo "${GH_TOKEN}" | docker login ghcr.io -u $ --password-stdin
 docker build --squash -t terrateam-base -f docker/terrat/Dockerfile.base .
 docker build --squash -t ghcr.io/terrateamio/terrateam:v1 -f docker/terrat/Dockerfile .
 docker push ghcr.io/terrateamio/terrateam:v1
