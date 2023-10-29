2b91a7593f74e1fc0d0ace08a72b51d736f97105
Malcolm <malcolm@terrateam.io>
2023-10-29 07:18:26 -0400

    PRO-427 ADD extra_args for s3 plan storage

--- code/src/terrat_repo_config/terrat_repo_config_storage_plan_s3.ml
+++ code/src/terrat_repo_config/terrat_repo_config_storage_plan_s3.ml
@@ -1,3 +1,11 @@
+module Delete_extra_args = struct
+  type t = string list [@@deriving yojson { strict = false; meta = true }, show, eq]
+end
+
+module Fetch_extra_args = struct
+  type t = string list [@@deriving yojson { strict = false; meta = true }, show, eq]
+end
+
 module Method = struct
   let t_of_yojson = function
     | `String "s3" -> Ok "s3"
@@ -7,13 +15,20 @@ module Method = struct
   [@@deriving yojson { strict = false; meta = true }, show, eq]
 end
 
+module Store_extra_args = struct
+  type t = string list [@@deriving yojson { strict = false; meta = true }, show, eq]
+end
+
 type t = {
   access_key_id : string option; [@default None]
   bucket : string;
+  delete_extra_args : Delete_extra_args.t option; [@default None]
   delete_used_plans : bool; [@default true]
+  fetch_extra_args : Fetch_extra_args.t option; [@default None]
   method_ : Method.t; [@key "method"]
   path : string option; [@default None]
   region : string;
   secret_access_key : string option; [@default None]
+  store_extra_args : Store_extra_args.t option; [@default None]
 }
 [@@deriving yojson { strict = true; meta = true }, make, show, eq]
