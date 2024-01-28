52289fd1384f63a2f9b3fb32d7b7620173fb05a4
Malcolm <malcolm@terrateam.io>
2024-01-28 18:43:03 +0100

    PRO-462 REFACTOR Sort function list

--- code/src/terrat/terrat_config.mli
+++ code/src/terrat/terrat_config.mli
@@ -11,25 +11,25 @@ module Telemetry : sig
     | Anonymous of Uri.t
 end
 
-val show_err : err -> string
+val admin_token : t -> string option
+val api_base : t -> string
 val create : unit -> (t, [> err ]) result
-val port : t -> int
-val db_host : t -> string
-val db_user : t -> string
-val db_password : t -> string
 val db : t -> string
+val db_connect_timeout : t -> float
+val db_host : t -> string
+val db_password : t -> string
+val db_user : t -> string
+val github_api_base_url : t -> Uri.t option
+val github_app_client_id : t -> string
+val github_app_client_secret : t -> string
 val github_app_id : t -> string
 val github_app_pem : t -> Mirage_crypto_pk.Rsa.priv
 val github_webhook_secret : t -> string option
-val github_app_client_secret : t -> string
-val github_app_client_id : t -> string
-val api_base : t -> string
-val python_exec : t -> string
-val infracost_pricing_api_endpoint : t -> Uri.t
 val infracost_api_key : t -> string
-val db_connect_timeout : t -> float
+val infracost_pricing_api_endpoint : t -> Uri.t
 val nginx_status_uri : t -> Uri.t option
-val admin_token : t -> string option
-val telemetry : t -> Telemetry.t
-val github_api_base_url : t -> Uri.t option
+val port : t -> int
+val python_exec : t -> string
+val show_err : err -> string
 val statement_timeout : t -> string
+val telemetry : t -> Telemetry.t
