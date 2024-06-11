7d31742a53bdeeaf7c3482b37c39467cb1e648e7
Malcolm Matalka <malcolm.matalka@acsl.se>
2024-06-11 12:34:25 -0400

    PRO-372 FIX Looking up drift work manifests assumed the schedule would still exist
    But this breaks if the schedule has been deleted

--- code/src/terrat_files/sql/select_github_drift_work_manifest.sql
+++ code/src/terrat_files/sql/select_github_drift_work_manifest.sql
@@ -1,9 +1,9 @@
 select
   gdwm.branch,
-  gds.reconcile
+  coalesce(gds.reconcile, false)
 from github_drift_work_manifests as gdwm
 inner join github_work_manifests as gwm
   on gwm.id = gdwm.work_manifest
-inner join github_drift_schedules as gds
+left join github_drift_schedules as gds
   on gds.repository = gwm.repository
 where gdwm.work_manifest = $work_manifest
