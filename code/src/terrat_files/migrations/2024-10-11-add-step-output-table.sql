d95af5f955d8c041e1ea402127b7a92227cf4632
Malcolm <malcolm@terrateam.io>
2024-10-11 18:25:48 +0100

    PRO-371 ADD Step output table

---
+++ code/src/terrat_files/migrations/2024-10-11-add-step-output-table.sql
@@ -0,0 +1,12 @@
+create table github_workflow_step_outputs (
+       created_at timestamp with time zone not null default now(),
+       idx smallint not null,
+       ignore_errors boolean not null,
+       payload jsonb not null,
+       scope jsonb not null,
+       step text not null,
+       success boolean not null,
+       work_manifest uuid not null,
+       primary key (work_manifest, scope, step),
+       foreign key (work_manifest) references github_work_manifests (id)
+);
