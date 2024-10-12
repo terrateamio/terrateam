c678081f4e19c27b92c5a5767e2d357c3c522c37
Malcolm <malcolm@terrateam.io>
2024-10-12 22:36:28 +0100

    PRO-371 REFACTOR Use Brtl_js2 pagination

--- code/src/terrat_ui_js/terrat_ui_js_comp_repos.ml
+++ code/src/terrat_ui_js/terrat_ui_js_comp_repos.ml
@@ -7,9 +7,9 @@ let comp state =
   let installation = Terrat_ui_js_state.selected_installation app_state in
   let module I = Terrat_api_components.Installation in
   let module Repo = Terrat_api_components.Installation_repo in
-  let module Page = Terrat_ui_js_comp_page.Make (struct
+  let module Page = Brtl_js2_page.Make (struct
     type fetch_err = Terrat_ui_js_client.err [@@deriving show]
-    type elt = Repo.t [@@deriving eq]
+    type elt = Repo.t [@@deriving eq, show]
     type state = Terrat_ui_js_state.t
     type query = { page : string list option } [@@deriving eq]
 
