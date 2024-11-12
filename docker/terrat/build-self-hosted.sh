a8e96e642ecf2ae520fdac67d65a0297b452e01a
BRR <bender@terrateam.io>
2024-11-12 20:33:00 +0100

    PRO-602 REFACTOR Use new terrat-oss Docker Image

--- docker/terrat/build-self-hosted.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eufx -o pipefail
-
-echo "${GH_TOKEN}" | docker login ghcr.io -u $ --password-stdin
-docker build --squash -t terrateam-base -f docker/terrat/Dockerfile.base .
-docker build --squash -t ghcr.io/terrateamio/terrateam:v1 -f docker/terrat/Dockerfile .
-docker push ghcr.io/terrateamio/terrateam:v1
