eb271d1010ecc11c300290195f2b74c1fbed4726
Malcolm <malcolm@terrateam.io>
2025-04-10 17:53:05 +0100

    #150 REFACTOR Separate VCS providers

---
+++ code/src/terrat_ui_js_service_github/terrat_ui_js_service_github.ml
@@ -0,0 +1,166 @@
+module At = Brtl_js2.Brr.At
+
+module Client = struct
+  module Http = Abb_js_fetch
+
+  module Io_err = struct
+    type t = {
+      name : string;
+      message : string;
+      stack : string;
+    }
+    [@@deriving show]
+
+    let of_js_error err =
+      {
+        name = Jstr.to_string (Jv.Error.name err);
+        message = Jstr.to_string (Jv.Error.message err);
+        stack = Jstr.to_string (Jv.Error.stack err);
+      }
+  end
+
+  type err =
+    [ `Conversion_err of string * string Openapi.Response.t
+    | `Missing_response of string Openapi.Response.t
+    | `Io_err of (Jv.Error.t[@printer fun fmt v -> Io_err.(pp fmt (of_js_error v))])
+    | `Forbidden
+    | `Not_found
+    ]
+  [@@deriving show]
+
+  type work_manifests_err =
+    [ err
+    | `Bad_request of Terrat_api_installations.List_work_manifests.Responses.Bad_request.t
+    ]
+  [@@deriving show]
+
+  type work_manifest_outputs_err =
+    [ err
+    | `Bad_request of Terrat_api_installations.Get_work_manifest_outputs.Responses.Bad_request.t
+    ]
+  [@@deriving show]
+
+  type dirspaces_err =
+    [ err
+    | `Bad_request of Terrat_api_installations.List_dirspaces.Responses.Bad_request.t
+    ]
+  [@@deriving show]
+
+  module Io = struct
+    type 'a t = 'a Abb_js.Future.t
+    type err = Jv.Error.t
+
+    let ( >>= ) = Abb_js.Future.Infix_monad.( >>= )
+    let return = Abb_js.Future.return
+
+    let call ?body ~headers ~meth url =
+      let url = Uri.to_string url in
+      let meth =
+        match meth with
+        | `Get -> `GET
+        | `Delete -> `DELETE
+        | `Patch -> assert false
+        | `Put -> `PUT
+        | `Post -> `POST
+      in
+      Http.fetch ?body ~headers ~meth ~url ()
+      >>= function
+      | Ok resp ->
+          return
+            (Ok
+               (Openapi.Response.make
+                  ~headers:(Http.Response.headers resp)
+                  ~status:(Http.Response.status resp)
+                  (Http.Response.text resp)))
+      | Error (`Js_err err) -> return (Error (`Io_err err))
+  end
+
+  module Api = Openapi.Make (Io)
+
+  module Page = struct
+    let parse_link s =
+      let open CCOption.Infix in
+      CCString.Split.left ~by:"; " s
+      >>= fun (link, rel) ->
+      let uri = Uri.of_string (String.sub link 1 (String.length link - 2)) in
+      CCString.Split.left ~by:"=" rel
+      >>= function
+      | "rel", n ->
+          let name = String.sub n 1 (String.length n - 2) in
+          Some (name, uri)
+      | _ -> None
+
+    let rec parse_links s =
+      if String.length s > 0 then
+        let open CCOption.Infix in
+        match CCString.Split.left ~by:", " s with
+        | Some (left, right) ->
+            parse_link left >>= fun link -> parse_links right >>= fun rest -> Some (link :: rest)
+        | None -> parse_link s >>= fun link -> Some [ link ]
+      else Some []
+
+    let of_response resp elts =
+      let headers = Openapi.Response.headers resp in
+      match CCList.Assoc.get ~eq:CCString.equal "link" headers with
+      | Some link -> (
+          match parse_links link with
+          | Some links ->
+              let next =
+                CCOption.flat_map
+                  (CCFun.flip Uri.get_query_param' "page")
+                  (CCList.Assoc.get ~eq:CCString.equal "next" links)
+              in
+              let prev =
+                CCOption.flat_map
+                  (CCFun.flip Uri.get_query_param' "page")
+                  (CCList.Assoc.get ~eq:CCString.equal "prev" links)
+              in
+              Brtl_js2_page.Page.make ?next ?prev elts
+          | None ->
+              Brtl_js2.Brr.Console.(log [ Jstr.v "Could not parse links"; Jstr.v link ]);
+              Brtl_js2_page.Page.make elts)
+      | None -> Brtl_js2_page.Page.make elts
+  end
+
+  type t = unit
+
+  let call = Api.call
+end
+
+type t = { client : Client.t }
+
+let create () = Abb_js.Future.return (Ok { client = () })
+
+module Comp = struct
+  module Login = struct
+    type config = Terrat_api_components.Server_config_github.t
+
+    let is_enabled =
+      let module C = Terrat_api_components.Server_config in
+      function
+      | { C.github } -> github
+
+    let run config state =
+      let module C = Terrat_api_components.Server_config_github in
+      Abb_js.Future.return
+        (Brtl_js2.Output.const
+           Brtl_js2.Brr.El.
+             [
+               a
+                 ~at:
+                   At.
+                     [
+                       href
+                         (Jstr.v
+                            (config.C.web_base_url
+                            ^ "/login/oauth/authorize?client_id="
+                            ^ config.C.app_client_id));
+                     ]
+                 [
+                   Tabler_icons_filled.brand_github ();
+                   div [ txt' "Login with GitHub" ];
+                   Tabler_icons_outline.arrow_narrow_right ();
+                 ];
+             ])
+  end
+end
