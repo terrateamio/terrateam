720fb9b3e7bcc2c9ca4b839f768a7094ea3a1c76
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-06-27 18:43:35 -0400

    wip

--- code/src/comment/comment.ml
+++ code/src/comment/comment.ml
@@ -1,17 +1,19 @@
-module type Id = sig
-  type t [@@deriving yojson, eq, show]
-
-  val of_string : string -> t option
-  val to_string : t -> string
-end
-
 module type Settings = sig
   type t = {
-    limit : int;
-    max_requests_limit : int;
+    max_length: int;
+    max_requests : int;
   }
 
   val default : unit -> t
+end
+
+module GithubSettings : Settings = struct
+  type t = {
+    max_length: int;
+    max_requests : int;
+  }
+
+  let default () = { max_length = 10; max_requests = 3}
 end
 
 module type Strategy = sig
@@ -19,64 +21,97 @@ module type Strategy = sig
     | Append
     | Delete
     | Minimize
+
+  val supported: unit -> t list
+end
+
+module GithubStrategy : Strategy = struct
+  type t =
+    | Append
+    | Delete
+    | Minimize
+
+  let supported () = [ Append ]
 end
 
 module type Output = sig
-  module Id : Id
-
   type t = {
-    id : Id.t;
+    id : string;
+    index: int;
+    content : string;
+  }
+end
+
+module Output : Output = struct
+  type t = {
+    id : string;
+    index: int;
     content : string;
   }
 end
 
 module type Protocol = sig
-  module O: Output
-  module Id : Id
+  module O : Output
   module S : Settings
   module St : Strategy
 
-  (** Breaks a sigle output into one or more chunks *)
-  val break : O.t -> S.t -> string list
+  type input
+  type 'a t
 
-  (** Combines multiple outputs while respecting a particular strategy. *)
-  val combine : S.t -> St.t -> string list -> string list
+  (** Breaks a sigle input into one or more chunks *)
+  val break : S.t -> input -> O.t t
+
+  (** Combines multiple outputs while respecting a
+      particular strategy. *)
+  val combine : S.t -> St.t -> O.t t -> O.t t
 end
 
-module Make (I : Id) (S : Settings) (St : Strategy) :
-  Protocol with module Id = I and module S = S and module St = St = struct
-  module Id = I
+module Make (O : Output) (S : Settings) (St : Strategy) : Protocol with
+  module S = S and
+  module St = St and
+  type input = string and
+  type 'a t = O.t list = struct
+
   module S = S
   module St = St
+  module O = O
 
-  let break id settings output =
-    let rec loop remaining id acc =
-      let limit = settings.S.limit in
-      let len = CCString.length remaining in
-      if len <= limit then CCList.append acc [ remaining ]
+  type input = string
+  type 'a t = O.t list
+
+  let break settings i =
+    let id = Ouuid.to_string (Ouuid.v4 ()) in
+    let limit = settings.S.max_length in
+    let rec loop remaining index acc =
+      if CCString.length remaining <= limit then 
+        let o: O.t = { id; index; content = remaining } in
+        acc @ [ o ]
       else
         let part = CCString.sub remaining 0 limit in
-        loop (CCString.drop limit remaining) id (CCList.append acc [ part ])
+        let o: O.t = { id; index; content = part } in
+        let updated = acc @ [ o ] in
+        loop (CCString.drop limit remaining) (index + 1) updated
     in
-    loop output id []
+    loop i 1 []
 
   let combine settings strategy outputs =
-    let id =
-      match Id.of_string "test" with
-      | Some id -> id
-      | None -> failwith "Could not create ID"
-    in
-    CCList.flat_map (break id settings) outputs
+    match strategy with
+    | St.Append -> []
+    | St.Delete -> []
+    | St.Minimize -> []
 end
 
 (*
 #require "containers";;
+#require "uuidm";;
+#mod_use "src/ouuid/ouuid.ml";;
 #mod_use "src/comment/comment.ml";;
 open Comment;;
 
-let s = Settings.{ limit = 10; max_requests = 10; };;
-let input1 = CCString.init 15 (fun _ -> 'a') ^ CCString.init 15 (fun _ -> 'b') ^ CCString.init 17 (fun _ -> 'c');;
-Make.break "ID" s input1;;
+let s = GithubSettings.default();;
+let c1 = CCString.init 15 (fun _ -> 'a') ^ CCString.init 15 (fun _ -> 'b') ^ CCString.init 17 (fun _ -> 'c');;
+module Github_protocol = Make(Output)(GithubSettings)(GithubStrategy);;
+Github_protocol.break s c1;;
 let input2 = [CCString.init 5 (fun _ -> 'a')] @ [CCString.init 15 (fun _ -> 'b')] @ [CCString.init 17 (fun _ -> 'c')];;
 Make.combine s input2;;
 *)
