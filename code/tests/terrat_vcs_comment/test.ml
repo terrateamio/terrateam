69f086874031828989a6f1e5c875a2092ac7afbe
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-07-02 18:19:21 -0400

    #561 ADD test types seem ok, fix module imports

--- code/tests/terrat_vcs_comment/test.ml
+++ code/tests/terrat_vcs_comment/test.ml
@@ -18,8 +18,10 @@ module Fake_db = struct
 
   let init () = { rows = [] }
 
-  let find_comment db comment_id =
+  let find_by_id db comment_id =
     List.find_opt (fun comment -> comment.comment_id = comment_id) db.rows
+
+  let find_by_status db status = List.filter (fun comment -> comment.status = status) db.rows
 end
 
 module Fake_api = struct
@@ -45,69 +47,72 @@ module Synthetic = struct
   type comment_id = int
 
   let query_comment_id t el =
+    let module F = Fake_db in
     Abb.Future.return
       (try
-         let c = CCList.find_opt (fun c -> CCList.mem el.id c.Fake_db.elements) t.db.rows in
+         let rows = t.db.F.rows in
+         let c = CCList.find_opt (fun c -> CCList.mem el.id c.F.elements) rows in
          match c with
-         | Some comment -> Ok (Some comment.comment_id)
+         | Some comment -> Ok (Some comment.F.comment_id)
          | None -> Ok None
        with _ -> Error `Error)
 
   let upsert_comment_id t els cid =
+    let module A = Fake_api in
+    let module F = Fake_db in
     Abb.Future.return
       (try
          let eids = CCList.map (fun el -> el.id) els in
          let content = CCString.concat "\n" (List.map (fun el -> el.content) els) in
 
-         match Fake_db.find_comment t.db cid with
+         match Fake_db.find_by_id t.db cid with
          | Some _ ->
              let updated_rows =
                CCList.map
                  (fun comment ->
-                   if comment.Fake_db.comment_id = cid then
-                     { comment with elements = eids; content }
+                   if comment.F.comment_id = cid then { comment with F.elements = eids; content }
                    else comment)
-                 t.db.rows
+                 t.db.F.rows
              in
-             t.db <- { rows = updated_rows };
+             t.db <- { F.rows = updated_rows };
              Ok ()
          | None ->
-             let new_id = t.api.next_id in
+             let new_id = t.api.A.next_id in
              let new_comment =
-               { Fake_db.comment_id = new_id; elements = eids; index = 1; content; status = Active }
+               { F.comment_id = new_id; F.elements = eids; F.index = 1; content; F.status = Active }
              in
-             t.api <- Fake_api.next t.api;
-             t.db <- { rows = new_comment :: t.db.rows };
+             t.api <- A.next t.api;
+             t.db <- { F.rows = new_comment :: t.db.F.rows };
              Ok ()
        with _ -> Error `Error)
 
   let delete_comment t cid =
+    let module F = Fake_db in
     Abb.Future.return
       (try
          let updated_rows =
            CCList.map
              (fun comment ->
-               if comment.Fake_db.comment_id = cid then { comment with status = Deleted }
-               else comment)
-             t.db.rows
+               if comment.F.comment_id = cid then { comment with F.status = Deleted } else comment)
+             t.db.F.rows
          in
 
-         t.db <- { rows = updated_rows };
+         t.db <- { F.rows = updated_rows };
          Ok ()
        with _ -> Error `Error)
 
   let minimize_comment t cid =
+    let module F = Fake_db in
     Abb.Future.return
       (try
          let updated_rows =
            CCList.map
              (fun comment ->
-               if comment.Fake_db.comment_id = cid then { comment with status = Minimized }
-               else comment)
-             t.db.rows
+               if comment.F.comment_id = cid then { comment with F.status = Minimized } else comment)
+             t.db.F.rows
          in
 
-         t.db <- { rows = updated_rows };
+         t.db <- { F.rows = updated_rows };
          Ok ()
        with _ -> Error `Error)
 
@@ -122,8 +127,11 @@ end
 
 let test_basic =
   Oth_abb.test ~name:"Basic Flow" (fun () ->
+      let module A = Fake_api in
+      let module F = Fake_db in
+      let module S = Synthetic in
       let module C = Terrat_vcs_comment.Make (Synthetic) in
-      let t : Synthetic.t = { api = Fake_api.init (); db = Fake_db.init () } in
+      let t = { S.api = A.init (); S.db = F.init () } in
       let open Abb.Future.Infix_monad in
       C.run t [] >>= fun _ -> Abb.Future.return ())
 
