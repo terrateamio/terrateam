0d18c5c8ef71e68cdd6e07e21c7506f0820ae1af
Malcolm <malcolm@terrateam.io>
2025-07-15 16:28:52 +0100

    #540 REFACTOR Move EE UI endpoints into common module

--- code/src/terrat_vcs_service_github/terrat_vcs_service_github_ep_user.mli
+++ code/src/terrat_vcs_service_github/terrat_vcs_service_github_ep_user.mli
@@ -2,3 +2,8 @@ module Whoami : sig
   val get :
     Terrat_vcs_service_github_provider.Api.Config.t -> Terrat_storage.t -> Brtl_rtng.Handler.t
 end
+
+module Installations : sig
+  val get :
+    Terrat_vcs_service_github_provider.Api.Config.t -> Terrat_storage.t -> Brtl_rtng.Handler.t
+end
