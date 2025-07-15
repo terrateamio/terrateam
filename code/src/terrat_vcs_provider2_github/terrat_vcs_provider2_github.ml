94f99ed61c2c8da843a6369b857361f1f0faa586
Malcolm <malcolm@terrateam.io>
2025-07-15 10:11:05 +0100

    #540 REFACTOR Simplify github provider code

--- code/src/terrat_vcs_provider2_github/terrat_vcs_provider2_github.ml
+++ code/src/terrat_vcs_provider2_github/terrat_vcs_provider2_github.ml
@@ -1,7 +1,13 @@
 module type S =
   Terrat_vcs_provider2.S
     with type Api.Account.Id.t = int
-     and type Api.Config.vcs_config = Terrat_config.Github.t
+     and type Api.Account.t = Terrat_vcs_api_github.Account.t
+     and type Api.Config.t = Terrat_vcs_api_github.Config.t
+     and type Api.Config.vcs_config = Terrat_vcs_api_github.Config.vcs_config
      and type Api.User.Id.t = string
+     and type Api.User.t = Terrat_vcs_api_github.User.t
      and type Api.Pull_request.Id.t = int
      and type Api.Repo.Id.t = int
+     and type Api.Repo.t = Terrat_vcs_api_github.Repo.t
+     and type Api.Ref.t = Terrat_vcs_api_github.Ref.t
+     and type Api.Client.t = Terrat_vcs_api_github.Client.t
--- code/src/terrat_vcs_service_github_ee/terrat_vcs_service_github_ee.ml
+++ code/src/terrat_vcs_service_github_ee/terrat_vcs_service_github_ee.ml
@@ -640,19 +640,8 @@ module Provider :
       Terrat_commit_check.make ~details_url ~description ~title ~status
   end
 
-  module Ui = struct
-    let work_manifest_url config account work_manifest =
-      let module Wm = Terrat_work_manifest3 in
-      Some
-        (Uri.of_string
-           (Printf.sprintf
-              "%s/i/%d/runs/%s"
-              (Uri.to_string (Terrat_config.terrateam_web_base_url @@ Api.Config.config config))
-              (Api.Account.id account)
-              (Uuidm.to_string work_manifest.Wm.id)))
-  end
-
-  module Comment = Terrat_vcs_service_github_provider.Comment (Ui)
+  module Ui = Terrat_vcs_service_github_provider.Ui
+  module Comment = Terrat_vcs_service_github_provider.Comment
 end
 
 module Routes = struct
