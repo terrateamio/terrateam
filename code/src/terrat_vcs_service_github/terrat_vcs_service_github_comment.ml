c75c81d20722ab893424ccfb938e4beda11adb9f
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-07-16 10:21:35 -0400

    #561 ADD start wiring things together

--- code/src/terrat_vcs_service_github/terrat_vcs_service_github_comment.ml
+++ code/src/terrat_vcs_service_github/terrat_vcs_service_github_comment.ml
@@ -1,5 +1,5 @@
 module S = struct
-  type t = { config : Terrat_vcs_service_github_provider.Api.Config.t }
+  type t = { client : Terrat_vcs_api_github.Client.t; }
 
   type el = {
     (* TODO: Check if we really need this *)
@@ -16,7 +16,10 @@ module S = struct
   let upsert_comment_id t els cid = Abb.Future.return (Ok ())
   let delete_comment t comment_id = Abb.Future.return (Ok ())
   let minimize_comment t comment_id = Abb.Future.return (Ok ())
-  let post_comment t els = Abb.Future.return (Ok 1)
+  let post_comment t els = 
+    let module Gh = Terrat_vcs_api_github in
+    let _request_id = "test" in
+    Abb.Future.return (Ok 1)
   let rendered_length els = CCList.fold_left (fun acc el -> acc + CCString.length el.content) 0 els
   let dirspace el = el.dirspace
   let is_success el = el.is_success
