ccfc5cc09229e7fa31907c6c8d498940e3e67395
Malcolm <malcolm@terrateam.io>
2025-07-16 13:15:30 +0100

    #150 FIX Make gitlab tables match github closer and fix a refactoring bug

--- code/src/terrat_files/github/sql/upsert_user_installations.sql
+++ code/src/terrat_files/github/sql/upsert_user_installations.sql
@@ -1,6 +1,22 @@
-insert into github_user_installations2 (
-  user_id,
-  installation_id
+with
+all_installation_ids as (
+    select t.id from unnest($installation_ids) as t(id)
+    inner join github_installations as gi
+        on gi.id = t.id
+),
+deleted as (
+    delete from github_user_installations2
+    where user_id = $user_id and not exists (select id from all_installation_ids)
+),
+inserted as (
+    insert into github_user_installations2 (user_id, installation_id)
+    select $user_id, id from all_installation_ids
+    on conflict (user_id, installation_id) do nothing
 )
-select * from unnest($user, $installation)
-on conflict (user_id, installation_id) do nothing
+select
+    gi.id,
+    gi.name,
+    gi.state
+from all_installation_ids as ai
+inner join github_installations as gi
+    on gi.id = ai.id
