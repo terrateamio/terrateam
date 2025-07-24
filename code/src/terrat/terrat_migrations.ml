c7eff3e47b66da346e09afc42a0050f000e345ec
Malcolm <malcolm@terrateam.io>
2025-07-24 23:51:43 +0100

    ADD New job tracking

--- code/src/terrat/terrat_migrations.ml
+++ code/src/terrat/terrat_migrations.ml
@@ -172,6 +172,7 @@ let migrations =
     ( "refactor-manage-github-maps-via-triggers",
       run_file_sql "2025-07-22-refactor-manage-github-maps-via-triggers.sql" );
     ("fix-gitlab-installation-state", run_file_sql "2025-07-21-fix-gitlab-installation-state.sql");
+    ("add-builder-tables", run_file_sql "2025-07-24-add-builder-tables.sql");
   ]
 
 let run config storage = Mig.run (config, storage) migrations
