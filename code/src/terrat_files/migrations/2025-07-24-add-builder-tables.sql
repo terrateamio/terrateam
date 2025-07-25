b3014e920966c49940bc2318f0c1619cc1f5bdd3
Malcolm <malcolm@terrateam.io>
2025-07-25 23:18:00 +0100

    #978 ADD Compute node

--- code/src/terrat_files/migrations/2025-07-24-add-builder-tables.sql
+++ code/src/terrat_files/migrations/2025-07-24-add-builder-tables.sql
@@ -42,3 +42,50 @@ create table job_work_manifests (
     foreign key (job_id) references jobs(id),
     foreign key (work_manifest) references work_manifests(id)
 );
+
+create table compute_node (
+    capabilities jsonb not null,
+    created_at timestamp with time zone default (now()) not null,
+    id uuid primary key,
+    repo uuid not null,
+    state text not null,
+    updated_at timestamp with time zone default (now()) not null
+);
+
+create table compute_node_work_states (
+    id text primary key
+);
+
+insert into compute_node_work_states values ('created'), ('delivered'), ('aborted');
+
+create table compute_node_work (
+    compute_node uuid,
+    created_at timestamp with time zone default (now()) not null,
+    requirements jsonb not null,
+    state text not null,
+    work jsonb not null,
+    work_manifest uuid primary key,
+    foreign key (work_manifest) references work_manifests(id),
+    foreign key (compute_node) references compute_node(id),
+    foreign key (state) references compute_node_work_states(id)
+);
+
+create unique index compute_node_work_wm_state_idx
+    on compute_node_work (work_manifest)
+    where state = 'created';
+
+-- Maintain that any aborted work manifests have their corresponding work aborted
+create or replace function trigger_update_compute_node_work_state()
+returns trigger as $$
+begin
+    if NEW.state = 'aborted' then
+        update compute_node_work set state = 'aborted'
+        where work_manifest = NEW.id;
+    end if;
+    return NEW;
+end;
+$$ language plpgsql;
+
+create trigger work_manifest_compute_node_work_state_trigger
+    after insert or update on work_manifests
+    execute function trigger_update_compute_node_work_state();
