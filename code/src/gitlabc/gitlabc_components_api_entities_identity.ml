820d6f10366fa6804296cf4bce01413c3bfd0327
Malcolm <malcolm@terrateam.io>
2025-08-07 20:57:32 +0100

    #690 FIX Authorization failure

--- code/src/gitlabc/gitlabc_components_api_entities_identity.ml
+++ code/src/gitlabc/gitlabc_components_api_entities_identity.ml
@@ -1,6 +1,5 @@
 type t = {
   extern_uid : string option; [@default None]
   provider : string option; [@default None]
-  saml_provider_id : string option; [@default None]
 }
 [@@deriving yojson { strict = false; meta = true }, show, eq]
