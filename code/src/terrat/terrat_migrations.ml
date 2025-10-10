39f28c468264a65647b34a0987a962dd2e50328e
Malcolm <malcolm@terrateam.io>
2025-10-10 08:08:00 +0200

    #208 ADD Bearer token database tables

--- code/src/terrat/terrat_migrations.ml
+++ code/src/terrat/terrat_migrations.ml
@@ -234,6 +234,7 @@ let migrations =
     ( "add-repo-created_at-index",
       run_file_sql ~mode:`Async "2025-09-23-add-repo-created_at-index.sql" );
     ("add-kv-store", run_file_sql "2025-09-17-add-kv-store.sql");
+    ("add-access-token-table", run_file_sql "2025-09-24-add-access-token-table.sql");
   ]
 
 let run config storage = Mig.run { Migrate.config; storage; tx = () } migrations
