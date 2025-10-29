8270991fd73b705bd374ae0125ef818fedf9013b
Aaron Sterren <aaron.sterren@bluevoyant.com>
2025-10-29 10:35:52 -0500

    949 FIX Update generated file from schema

--- code/src/gitlabc/gitlabc_components_postapiv4projectsidpipeline.ml
+++ code/src/gitlabc/gitlabc_components_postapiv4projectsidpipeline.ml
@@ -1,9 +1,9 @@
 module Inputs = struct
-    include Json_schema.Additional_properties.Make (Json_schema.Empty_obj) (Json_schema.Obj)
+  include Json_schema.Additional_properties.Make (Json_schema.Empty_obj) (Json_schema.Obj)
 end
 
 type t = {
-  ref_ : string; [@key "ref"]
   inputs : Inputs.t option; [@default None]
+  ref_ : string; [@key "ref"]
 }
 [@@deriving yojson { strict = false; meta = true }, show, eq]
