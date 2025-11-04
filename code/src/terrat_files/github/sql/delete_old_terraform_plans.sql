b23254769c261dd45fd904f510519f7bf8b0b5fb
Malcolm <malcolm@terrateam.io>
2025-11-04 14:57:13 +0000

    #921 FIX Limit how many plans can be modified in one go

--- code/src/terrat_files/github/sql/delete_old_terraform_plans.sql
+++ code/src/terrat_files/github/sql/delete_old_terraform_plans.sql
@@ -9,11 +9,20 @@ with deleted_plans as (
 -- the current system.  However, for those plan rows that we want to keep
 -- around, we need to empty the data so that we don't store it and it takes up
 -- less space.
+empty_plans_limited as (
+    select
+        gwm.id as id
+    from github_work_manifests as gwm
+    inner join plans as p
+        on p.work_manifest = gwm.id
+    where gwm.created_at < now() - interval '14 days' and not p.has_changes and data <> ''
+    limit 5000
+),
 empty_plans as (
     update plans as p
         set data = ''
-    from github_work_manifests as gwm
-    where p.work_manifest = gwm.id and gwm.created_at < now() - interval '14 days' and not p.has_changes and data <> ''
+    from empty_plans_limited as e
+    where p.work_manifest = e.id
     returning (p.work_manifest, p.path, p.workspace)
 ),
 all_updates as (
