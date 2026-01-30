f5ec52e8cf274cd2e88d6f1ce1b1a9575ea0d8ba
Tamiya <tamiyasigmar@gmail.com>
2026-01-30 14:57:22 +0100

    #1119 ADD Add Sln_set.{pp,show}

--- code/src/sln/sln_set.ml
+++ code/src/sln/sln_set.ml
@@ -1,11 +1,15 @@
 module String = struct
   include CCSet.Make (CCString)
 
+  type t_list = string list [@@deriving show]
+
   let to_yojson t = to_list t |> [%to_yojson: string list]
 
   let of_yojson ls =
     let open CCResult.Infix in
     [%of_yojson: string list] ls >|= of_list
 
+  let pp fmt t = pp_t_list fmt (to_list t)
+  let show t = show_t_list (to_list t)
   let dedup_list l = l |> of_list |> to_list
 end
