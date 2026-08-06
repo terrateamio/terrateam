3be51f362b30bf0674225ba1f02f3d77135278cb
Tamiya <254410394+tamiyasigmar@users.noreply.github.com>
2026-08-06 13:54:03 +0200

    Staging promotion (at PR #11 (mono-export-2026-08-06-11_49_30))
    Monorepo export 2026-08-06-11_49_30
    
    GitOrigin-RevId: a55a829eea0149cd431caf44e4713d598b433199

--- code/src/iris/src/lib/constants.ts
+++ code/src/iris/src/lib/constants.ts
@@ -12,8 +12,8 @@ export const WEB3_FORMS_ACCESS_KEY = '8d8c30e4-965e-4f9c-aa8e-9a7e8de01f11';
 export const EXTERNAL_URLS = {
   DOCS: 'https://docs.terrateam.io',
   SLACK: 'https://terrateam.io/slack',
-  GITHUB_ISSUES: 'https://github.com/terrateamio/terrateam/issues',
-  GITHUB_DISCUSSIONS: 'https://github.com/terrateamio/terrateam/discussions',
+  GITHUB_ISSUES: 'https://github.com/stategraph/stategraph/issues',
+  GITHUB_DISCUSSIONS: 'https://github.com/stategraph/stategraph/discussions',
   CALENDLY: 'https://calendly.com/terrateam/30-minute-chat',
   STATUS_PAGE: 'https://status.terrateam.io/',
   WEB3_FORMS_ENDPOINT: 'https://api.web3forms.com/submit'
